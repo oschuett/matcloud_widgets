21b3e3a65601e506d5c52432bd76e1c0ae67e985
Ole Schuett <ole.schuett@empa.ch>
2017-11-30 12:01:21 +0100

    leaving Empa, work in progress

--- matcloud_widgets/static/main.js
+++ matcloud_widgets/static/main.js
@@ -1,35 +1,3 @@
-// toggle display of all code cells' inputs
-
-//define([
-//    'jquery',
-//    'base/js/namespace',
-//    'base/js/dialog',
-//    'base/js/events',
-//    'base/js/utils',
-//    'require',
-//], function(
-//    $,
-//    Jupyter,
-//    dialog,
-//    events,
-//    utils,
-//    require
-//) {
-//    "use strict";
-//
-//    
-//    ////==========================================================================
-//    //var load_ipython_extension = function() {
-//    //    alert("matcloud-widgets loaded");
-//    //};
-//    //
-//    ////==========================================================================
-//    //return {
-//    //    load_ipython_extension : load_ipython_extension
-//    //};
-//});
-
-
 define([
     'jquery',
     'require',
@@ -38,15 +6,11 @@ define([
     require
 ) {
     "use strict";
-
     
-    //==========================================================================
     var load_ipython_extension = function() {
-    //    alert("matcloud-widgets loaded");
-    // TODO: inject CSS
+        // do nothing
     };
     
-    ////==========================================================================
     return {
         load_ipython_extension : load_ipython_extension
     };
@@ -58,14 +22,14 @@ define('matcloud_widgets', ["jupyter-js-widgets"], function(widgets) {
     var InfoBubbleView = widgets.DOMWidgetView.extend({
 
         render: function() {
-            $('<i class="fa fa-question-circle"></i>').appendTo(this.el);
+            $('<i class="fa fa-question-circle"></i>').appendTo(this.$el);
             this.value_changed();
             this.model.on('change:value', this.value_changed, this);    
         },
 
         value_changed: function() {
             var txt = this.model.get('value')
-            $(this.el).children().attr("title", txt);
+            this.$el.children("i").attr("title", txt);
         },
     });
 
@@ -73,22 +37,3 @@ define('matcloud_widgets', ["jupyter-js-widgets"], function(widgets) {
         InfoBubbleView : InfoBubbleView
     };
 });
-
-// define('hello', ["jupyter-js-widgets"], function(widgets) {
-// 
-//     var HelloView = widgets.DOMWidgetView.extend({
-// 
-//         render: function() {
-//             this.value_changed();
-//             this.model.on('change:value', this.value_changed, this);
-//         },
-// 
-//         value_changed: function() {
-//             this.el.textContent = this.model.get('value');
-//         },
-//     });
-// 
-//     return {
-//         HelloView : HelloView
-//     };
-// });
